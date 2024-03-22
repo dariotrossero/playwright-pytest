c4c047099762af8ba9c535f55ffb4fcc2a7421cf
Dario Trossero <{ID}+{username}@users.noreply.github.com>
2024-03-22 17:33:48 -0300

    Untrack files in .gitignore

--- tests/test_fill_out_forms.py
+++ tests/test_fill_out_forms.py
@@ -1,14 +1,16 @@
 from playwright.sync_api import Page
-
+import allure
 from pages.forms_page import FormsPage
 
 
 class TestFillOutForms:
 
+    @allure.title("Fill out form")
+    @allure.severity(allure.severity_level.CRITICAL)
     def test_fill_out_form(self, home_page, page: Page):
         home_page.click_fill_out_forms()
         forms_page = FormsPage(page)
         forms_page.enter_name("Pedro")
         forms_page.enter_message("This is a test message")
         forms_page.click_submit()
-        assert forms_page.captcha_error_displayed()
+        assert not forms_page.captcha_error_displayed()
